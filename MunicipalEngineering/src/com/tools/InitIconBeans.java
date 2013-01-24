3a54300991991d45c7d6095b91e2a18118e7e7f2
345945988@qq.com <345945988@qq.com>
2013-01-24 17:08:50 +0800

    fix

--- MunicipalEngineering/src/com/tools/InitIconBeans.java
+++ MunicipalEngineering/src/com/tools/InitIconBeans.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 
 import com.municipalengineering.activity.R;
 import com.municipalengineering.entity.IconBean;
+import com.municipalengineering.fragment.CompanyInquiryFragment;
+import com.municipalengineering.fragment.PersonnelInquiryFragment;
+import com.municipalengineering.fragment.TendersInquiryFragment;
+import com.municipalengineering.fragment.TendersInspectionFragment;
 import com.municipalengineering.view.CompanyInquiryView;
 import com.municipalengineering.view.HomeView;
 import com.municipalengineering.view.PersonnelInquiryView;
@@ -20,17 +24,17 @@ public class InitIconBeans {
         //this should init from the native file  to allow the location modify
 		mHandleIncos.add(new IconBean(HomeView.class.getName(),
 				R.drawable.icon_home));
-		mHandleIncos.add(new IconBean(TendersInspectionView.class.getName(),
+		mHandleIncos.add(new IconBean(TendersInspectionFragment.class.getName(),
 				R.drawable.icon_bdxj));
 
 		// ==================init the home icons==================
-		mHomeIcons.add(new IconBean(TendersInspectionView.class.getName(),
+		mHomeIcons.add(new IconBean(TendersInspectionFragment.class.getName(),
 				R.drawable.icon_bdxj));
-		mHomeIcons.add(new IconBean(TendersInquiryView.class.getName(),
+		mHomeIcons.add(new IconBean(TendersInquiryFragment.class.getName(),
 				R.drawable.icon_bdcx));
-		mHomeIcons.add(new IconBean(CompanyInquiryView.class.getName(),
+		mHomeIcons.add(new IconBean(CompanyInquiryFragment.class.getName(),
 				R.drawable.icon_gscx));
-		mHomeIcons.add(new IconBean(PersonnelInquiryView.class.getName(),
+		mHomeIcons.add(new IconBean(PersonnelInquiryFragment.class.getName(),
 				R.drawable.icon_rycx));
 	};
 
